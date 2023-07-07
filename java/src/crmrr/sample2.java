a4a98950a7700e2bd6485a57b9eea0e8ca86e5a5
shiva <shivakumarb5050@gmail.com>
2023-07-08 00:17:49 +0530

    changes in java

---
+++ java/src/crmrr/sample2.java
@@ -0,0 +1,12 @@
+package crmrr;
+
+public interface sample2 {
+	
+		default  void m2() {
+			System.out.println("m1 method");
+			
+		}
+		
+		
+
+}
---
+++ java/src/crmrr/sasjvghg.java
@@ -0,0 +1,22 @@
+package crmrr;
+
+ class sasjvghg  implements sample1,sample2 {
+	
+	
+	
+	public static void main(String[] args) {
+		sasjvghg s= new sasjvghg();
+		s.m2();
+	
+}
+
+	@Override
+	public void m2() {
+		// TODO Auto-generated method stub
+		sample1.super.m2();
+	}
+
+	
+
+	
+}
